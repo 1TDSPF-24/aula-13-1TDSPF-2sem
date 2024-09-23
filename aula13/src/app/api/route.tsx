a74c39cc26ff901e9e1744042ccb4ba555e17e8d
joaolps <joaocoelho69096@gmai.com>
2024-09-23 09:41:46 -0300

    salvando para teste de API

---
+++ aula13/src/app/api/route.tsx
@@ -0,0 +1,13 @@
+import { promises as fs } from "fs";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request, { params }: { params: { id: string } }) {
+    try {
+        const file = await fs.readFile(process.cwd() + '/src/app/base/db.json', 'utf8');
+        return NextResponse.json(JSON.parse(file));
+    }
+    catch (error) {
+        console.error("Erro ao ler o Arquivo Json", error)
+        return NextResponse.json({ messafe: "Erro ao ler o arquivo produtos" })
+    }
+}
