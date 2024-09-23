2763b42b44b7468899e1a9be00c718b15a6703df
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-09-23 08:29:42 -0300

    PAssagem de parâmetros por props...

--- aula13/src/app/produto/[id]/page.tsx
+++ aula13/src/app/produto/[id]/page.tsx
@@ -1,14 +1,14 @@
-"use client";
-import { useParams } from "next/navigation"
+type TipoParametro = {
+  id:number;
+}
 
-export default function Produto() {
 
-    const {id} = useParams();
+export default function Produto({params}:{params:TipoParametro}) {
 
   return (
     <div>
         <h2>Produto</h2>
-        <p>Produto {id}</p>
+        <p>Produto {params.id}</p>
     </div>
   )
 }
