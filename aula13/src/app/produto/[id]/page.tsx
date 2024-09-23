7f3152e63df4ebdb503c076e83acfeacfb2b2276
dugiovannini <egiovannini5@gmail.com>
2024-09-23 09:52:27 -0300

    Aula 13 de next

--- aula13/src/app/produto/[id]/page.tsx
+++ aula13/src/app/produto/[id]/page.tsx
@@ -1,14 +1,9 @@
-"use client";
-import { useParams } from "next/navigation"
-
 export default function Produto() {
-
-    const {id} = useParams();
 
   return (
     <div>
-        <h2>Produto</h2>
-        <p>Produto {id}</p>
+      <h2>Produto</h2>
+      <p>Produto</p>
     </div>
   )
 }
