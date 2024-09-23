231be69e91686b874859f6565f1ca8218d911f14
cadenasza <leonardocadena2005@gmail.com>
2024-09-23 09:52:05 -0300

    Criando API com router Handler. NEXTRequest e NEXTResponse

--- aula13/src/app/produto/[id]/page.tsx
+++ aula13/src/app/produto/[id]/page.tsx
@@ -1,13 +1,9 @@
-type TipoParametro = {
-  id: number;
-}
-
-export default function Produto({ params }: { params: TipoParametro }) {
+export default function Produto() {
 
   return (
     <div>
       <h2>Produto</h2>
-      <p>Produto {params.id}</p>
+      <p>Produto </p>
     </div>
   )
 }
--- aula13/src/app/produtos/page.tsx
+++ aula13/src/app/produtos/page.tsx
@@ -3,18 +3,11 @@ import Link from "next/link";
 
 export default function Produtos() {
 
-    const listaProdutos = [
-        {id: 1, nome: 'Coca-Cola', preco: 5.00},
-        {id: 2, nome: 'Fanta', preco: 4.00},
-        {id: 3, nome: 'Guarana', preco: 4.50},
-        {id: 4, nome: 'Refrigerante', preco: 5.34}
-    ];
-
   return (
     <div>
         <h2>Produtos</h2>
 
-            <table className="custom-table">
+            {/* <table className="custom-table">
                 <thead>
                     <tr>
                         <th>Id</th>
@@ -40,8 +33,7 @@ export default function Produtos() {
                         </td>
                     </tr>
                 </tfoot>
-            </table>
-
+            </table> */}
     </div>
   )
 }
