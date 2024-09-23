53b51de2156addb1acea38f3ab8aa5c080a1d06d
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-23 09:03:33 -0300

    aa

--- aula13/src/app/produtos/page.tsx
+++ aula13/src/app/produtos/page.tsx
@@ -1,18 +1,11 @@
 
-import Link from "next/link";
 
 export default function Produtos() {
 
-    const listaProdutos = [
-        {id: 1, nome: 'Coca-Cola', preco: 5.00},
-        {id: 2, nome: 'Fanta', preco: 4.00},
-        {id: 3, nome: 'Guarana', preco: 4.50},
-        {id: 4, nome: 'Refrigerante', preco: 5.34}
-    ];
 
-  return (
-    <div>
-        <h2>Produtos</h2>
+    return (
+        <div>
+            <h2>Produtos</h2>
 
             <table className="custom-table">
                 <thead>
@@ -24,24 +17,17 @@ export default function Produtos() {
                     </tr>
                 </thead>
                 <tbody>
-                    {listaProdutos.map((p)=>(
-                        <tr key={p.id}>
-                            <td>{p.id}</td>
-                            <td>{p.nome}</td>
-                            <td>{p.preco}</td>
-                            <td> <Link href={`/produto/${p.id}`}>Editar</Link> </td>
-                        </tr>
-                    ))}
+
                 </tbody>
                 <tfoot>
                     <tr>
                         <td colSpan={4}>
-                            <h2>Quantidade de registros:{listaProdutos.length}</h2>
+                            <h2></h2>
                         </td>
                     </tr>
                 </tfoot>
             </table>
 
-    </div>
-  )
+        </div>
+    )
 }
